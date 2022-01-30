20d1f7f6345f5f1e30317028d75bdfa3c6b9551d
Khalmatov <large000@yandex.ru>
2022-01-30 17:19:09 +0300

    Добавьте файлы проекта.

---
+++ Module_18_patterns_part_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_18_patterns_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
